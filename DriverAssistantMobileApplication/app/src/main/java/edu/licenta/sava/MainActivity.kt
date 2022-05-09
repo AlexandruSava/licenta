30eb3ccb0ba3589c4cdec8d296b0e2052ed4051d
Alexandru Sava <alexandru.sava@brivo.com>
2022-05-09 23:32:47 +0300

    add basic login page

--- DriverAssistantMobileApplication/app/src/main/java/edu/licenta/sava/MainActivity.kt
+++ DriverAssistantMobileApplication/app/src/main/java/edu/licenta/sava/MainActivity.kt
@@ -2,10 +2,36 @@ package edu.licenta.sava
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import edu.licenta.sava.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        initializeButtons()
+    }
+
+    fun initializeButtons() {
+        cancelAction()
+
+        nextAction()
+    }
+
+    private fun nextAction() {
+        binding.nextButton.setOnClickListener {
+
+        }
+    }
+
+    private fun cancelAction() {
+        binding.cancelButton.setOnClickListener {
+
+        }
     }
 }
