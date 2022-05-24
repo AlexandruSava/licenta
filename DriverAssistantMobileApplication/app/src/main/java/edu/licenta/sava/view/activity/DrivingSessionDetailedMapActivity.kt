dd02ccf034c4613b268d5f9a91aa7b3af389a800
AlexandruSava <alexandru.sava.1999@gmail.com>
2022-05-24 23:12:30 +0300

    Add Driving Session Detailed Map Activity with details about path followed and warnings issued during a finished driving session

---
+++ DriverAssistantMobileApplication/app/src/main/java/edu/licenta/sava/view/activity/DrivingSessionDetailedMapActivity.kt
@@ -0,0 +1,162 @@
+package edu.licenta.sava.view.activity
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import edu.licenta.sava.R
+import edu.licenta.sava.database.DatabaseController
+import edu.licenta.sava.databinding.ActivityDrivingSessionDetailedMapBinding
+import edu.licenta.sava.model.DrivingSession
+
+class DrivingSessionDetailedMapActivity : AppCompatActivity() {
+
+    private val databaseController = DatabaseController()
+
+    private lateinit var binding: ActivityDrivingSessionDetailedMapBinding
+    private lateinit var drawer: DrawerLayout
+
+    private lateinit var userId: String
+    private lateinit var email: String
+    private var endTime: Long = 0L
+
+    private lateinit var currentDrivingSession: DrivingSession
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setBinding()
+        setUserAndEmail()
+        initializeToolbarAndMenu()
+        initializeButtons()
+        getStorageData()
+    }
+
+    private fun initializeButtons() {
+        binding.backBtn.setOnClickListener {
+            onBackPressed()
+            finish()
+        }
+    }
+
+    private fun setBinding() {
+        binding = ActivityDrivingSessionDetailedMapBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    private fun setUserAndEmail() {
+        val userIdString = intent.getStringExtra("userId")
+        val emailString = intent.getStringExtra("email")
+        val endTimeLong = intent.getLongExtra("endTime", 0L)
+        if (!userIdString.isNullOrEmpty() && !emailString.isNullOrEmpty()) {
+            userId = userIdString
+            email = emailString
+            endTime = endTimeLong
+        }
+    }
+
+    private fun initializeToolbarAndMenu() {
+        val toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+
+        drawer = binding.drawer
+        drawer.addDrawerListener(
+            ActionBarDrawerToggle(
+                this,
+                drawer,
+                toolbar,
+                R.string.show_navigation,
+                R.string.hide_navigation
+            )
+        )
+
+        val navigation = binding.navigationView
+        navigation.setNavigationItemSelectedListener { menuItem ->
+            onOptionsItemSelected(menuItem)
+            navigation.setCheckedItem(menuItem)
+            drawer.closeDrawer(GravityCompat.START)
+            true
+        }
+
+        binding.navigationView
+            .getHeaderView(0)
+            .findViewById<TextView>(R.id.email_menu)
+            .text = email
+
+        navigation.setCheckedItem(R.id.history_item)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.logout_item) {
+            logoutAction()
+            return true
+        }
+        if (item.itemId == R.id.history_item) {
+            historyAction()
+            return true
+        }
+        if (item.itemId == R.id.dashboard_item) {
+            dashboardAction()
+            return true
+        }
+        if (item.itemId == R.id.start_session_item) {
+            startSessionAction()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun logoutAction() {
+        val intent = Intent(this, LoginActivity::class.java)
+        Firebase.auth.signOut()
+        startActivity(intent)
+        finish()
+    }
+
+    private fun historyAction() {
+        val intent = Intent(this, DrivingSessionsHistoryActivity::class.java)
+        Firebase.auth.signOut()
+        startActivity(intent)
+        finish()
+    }
+
+    private fun dashboardAction() {
+        val intent = Intent(this, DashboardActivity::class.java)
+        intent.putExtra("userId", userId)
+        intent.putExtra("email", email)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun startSessionAction() {
+        val intent = Intent(this, DrivingSessionActivity::class.java)
+        intent.putExtra("userId", userId)
+        intent.putExtra("email", email)
+        startActivity(intent)
+        finish()
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun getStorageData() {
+        val initialized = databaseController.verifyPresenceOfALocalFile(this, userId)
+        if (initialized) {
+            currentDrivingSession =
+                databaseController.getDrivingSessionBySessionEndTime(
+                    this,
+                    userId,
+                    endTime
+                )
+        }
+    }
+
+    fun getCurrentDrivingSession(): DrivingSession {
+        return currentDrivingSession
+    }
+}
---
+++ DriverAssistantMobileApplication/app/src/main/java/edu/licenta/sava/view/maps/MapsFragment.kt
@@ -0,0 +1,105 @@
+package edu.licenta.sava.view.maps
+
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
+import edu.licenta.sava.R
+import edu.licenta.sava.model.DrivingSession
+import edu.licenta.sava.view.activity.DrivingSessionDetailedMapActivity
+
+class MapsFragment : Fragment() {
+
+    private lateinit var drivingSession: DrivingSession
+
+    private val callback = OnMapReadyCallback { googleMap ->
+
+        val warningEventsList = drivingSession.warningEventsList
+        val sensorDataList = drivingSession.sensorDataList
+
+        val start = LatLng(
+            sensorDataList.first().latitude,
+            sensorDataList.first().longitude
+        )
+        val stop = LatLng(
+            sensorDataList.last().latitude,
+            sensorDataList.last().longitude
+        )
+
+        val latLngList = ArrayList<LatLng>()
+        latLngList.add(start)
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(start)
+                .title("START")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+        )?.tag = 1
+
+        for (warningEvent in warningEventsList) {
+            val latitude = warningEvent.sensorData.latitude
+            val longitude = warningEvent.sensorData.longitude
+            val message =
+                "Speed: " + warningEvent.sensorData.speed.toString() + "KM/H, Limit: " + warningEvent.sensorData.speedLimit + "KM/H"
+            val latLng = LatLng(latitude, longitude)
+            googleMap.addMarker(
+                MarkerOptions()
+                    .position(latLng)
+                    .title(message)
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
+            )?.tag = 0
+        }
+
+        for (sensorData in sensorDataList) {
+            val latLng = LatLng(sensorData.latitude, sensorData.longitude)
+            latLngList.add(latLng)
+        }
+
+        latLngList.add(stop)
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(stop)
+                .title("STOP")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+        )?.tag = 2
+
+        googleMap.addPolyline(
+            PolylineOptions()
+                .color(Color.DKGRAY)
+                .clickable(true)
+                .addAll(latLngList)
+                .width(10F)
+        ).tag = 1
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(start))
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(14.0f))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.maps_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val drivingSessionActivity = activity as DrivingSessionDetailedMapActivity
+        drivingSession = drivingSessionActivity.getCurrentDrivingSession()
+        Log.d("Fragment:", "I acquired the following driving session $drivingSession")
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
+}
